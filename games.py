999073bdadaf428910a30fc37f06eaed6dd4a70c
Manvender Singh <151474058+manvendersingh21@users.noreply.github.com>
2023-11-27 18:22:56 -0500

    Add files via upload

--- games.py
+++ games.py
@@ -11,11 +11,7 @@ import random
 shufflecards1=[]
 shufflecards2=[]
 shufflecards3=[]
-shufflecards4=[]
-
-        
-
-    
+shufflecards4=[]    
 pygame.init()
 #set Game resolution
 screen = pygame.display.set_mode((1000,600))
@@ -27,34 +23,14 @@ clock=pygame.time.Clock()
 font = pygame.font.Font('fontsss/Pixeltype.ttf', 50)
 #calling picture in the Game
 Table_top=pygame.image.load('PNG/table.jpg').convert()#convert_alpha for cards
-
 Writing= Writing= font.render('CallBreak',False,'Black')
 start= font.render('Click anywhere or Press any key on the screen to start the game',False,'Black')
-
 bot1=pygame.image.load("PNG/back.png")
 bot1=pygame.transform.rotate(bot1,-90) 
 w=bot1.get_width()
 b=bot1.get_height()
 bot=pygame.transform.scale(bot1,(w*0.04,b*0.04)).convert_alpha()
 screen.blit(Table_top,(0,0))
-class Player():#pygame.sprite.Sprite):
-	def __init__(self, x, y, botcards,cardnum  ):
-		#super().__init__()
-		self.dis='shufflecards{}[{}]'.format(botcards, cardnum)
-		self.image=pygame.image.load('PNG/'+self.dis)
-		self.scale=pygame.transform.scale(self.image,((self.image.get_width())*0.09,(self.image.get_height())*0.09)).convert_alpha()
-		self.display=screen.blit(self.scale,self.scale.get_rect(midbottom=((x),y)))
-		
-		
-
-def Gameplay(N):
-    x=0
-    suits=['C','S','D','H']
-    print(shufflecards2,'hgh')
-    for card in shufflecards2:
-        if shufflecards1[N][1:2]==card[1:2]:
-            cardsdis=Player(500,300,2,N)
-            cardsdis.display
 def cardsShuffle():
     cards=['2C.png', '3C.png', '4C.png', '5C.png', '6C.png', '7C.png', '8C.png', '9C.png', '10C.png','2D.png', '3D.png', '4D.png', '5D.png', '6D.png', '7D.png', '8D.png', '9D.png', '10D.png',
            '2S.png', '3S.png', '4S.png', '5S.png', '6S.png', '7S.png', '8S.png', '9S.png', '10S.png','2H.png', '3H.png', '4H.png', '5H.png', '6H.png', '7H.png', '8H.png', '9H.png', '10H.png',
@@ -63,23 +39,41 @@ def cardsShuffle():
     
     
     shufflecards1=cards[:13]
+    
     shufflecards1.sort()
     shufflecards2=cards[13:26]
+    
     shufflecards3=cards[26:39]
     shufflecards4=cards[39:]
-    return shufflecards1,shufflecards2,shufflecards3,shufflecards4
-        
-            
-
     
-
-
-
     
+    return shufflecards1,shufflecards2,shufflecards3,shufflecards4  
+
+
+class Player():pygame.sprite.Sprite):
+        shufflecards1,shufflecards2,shufflecards3,shufflecards4=cardsShuffle()
+        def __init__(self, x, y, botcards,cardnum  ):
+                self.dis='shufflecards{}[{}]'.format(botcards, cardnum)
+                self.image=pygame.image.load('PNG/'+self.dis)
+                self.scale=pygame.transform.scale(self.image,((self.image.get_width())*0.09,(self.image.get_height())*0.09)).convert_alpha()
+        def display():
+                screen.blit(self.scale,self.scale.get_rect(midbottom=((x),y)))
+		
+		
+
+def Gameplay(N): 
+    x=0
+    suits=['C','S','D','H']
+    shufflecards1,shufflecards2,shufflecards3,shufflecards4=cardsShuffle()
+    for card in shufflecards2:
+        if shufflecards1[N][1]==card[1]:
+            cardsdis=Player(500,300,2,N)
+            cardsdis.display()
+
 def maingame():
     a='j'
     
-    cardsShuffle()
+    shufflecards1,shufflecards2,shufflecards3,shufflecards4=cardsShuffle()
     
     
     rect=[]
